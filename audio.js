836776972e8b36a651248d625650a91714129b28
Judith Fashipe <fasipejudith@gmail.com>
2022-10-30 15:31:21 -0700

    Changed the audio source to United Nations

--- audio.js
+++ audio.js
@@ -9,31 +9,31 @@ Amplitude.init({
       {
          "name":"Causes Of Climate Change",
          "url":"./audio/Causes of climate change.mp3",
-         "source": "World Health Organization",
+         "source": "United Nations",
          "source_url": "https://www.who.int/publications/i/item/9789240003927"
       },
       {
          "name":"How Climate Change Has Disrupted The Environment",
          "url":"./audio/How climate change has disrupted the environment 1.mp3",
-         "source": "World Health Organization",
+         "source": "United Nations",
          "source_url": "https://www.who.int/publications/i/item/9789240003927"
       },
       {
          "name":"How Climate Change Has Disrupted The Environment",
          "url":"./audio/How climate change has disrupted the environment 2.mp3",
-         "source": "World Health Organization",
+         "source": "United Nations",
          "source_url": "https://www.who.int/publications/i/item/9789240003927"
       },
       {
          "name":"Health and Climate Change",
          "url":"./audio/Health and Climate Change.mp3",
-         "source": "World Health Organization",
+         "source": "United Nations",
          "source_url": "https://www.who.int/publications/i/item/9789240003927"
       },
       {
          "name":"How Can We Fight Climate Change",
          "url":"./audio/How can we fight climate change.mp3",
-         "source": "World Health Organization",
+         "source": "United Nations",
          "source_url": "https://www.who.int/publications/i/item/9789240003927"
       },
       

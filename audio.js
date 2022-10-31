a00eaa9a520da92e974146783ad6bcf3a0e69356
fadeke-o <fadekemiogundiya@gmail.com>
2022-10-31 18:34:52 +0100

    Made changes to the names of the audio url

--- audio.js
+++ audio.js
@@ -4,43 +4,43 @@ Amplitude.init({
          "name":"What Is Climate Change?",
          "url":"./audio/What_is_climate_change.mp3",
          "source": "United Nations",
-         "source_url": "https://www.who.int/publications/i/item/9789240003927"
+         "source_url": "https://edenconserve.org/about-us/"
       },
       {
          "name":"Causes Of Climate Change",
          "url":"./audio/Causes of climate change.mp3",
          "source": "United Nations",
-         "source_url": "https://www.who.int/publications/i/item/9789240003927"
+         "source_url": "https://edenconserve.org/about-us/"
       },
       {
          "name":"How Climate Change Has Disrupted The Environment",
-         "url":"./audio/How climate change has disrupted the environment 1.mp3",
+         "url":"./audio/How Climate change has disrupted the environment 1.mp3",
          "source": "United Nations",
-         "source_url": "https://www.who.int/publications/i/item/9789240003927"
+         "source_url": "https://edenconserve.org/about-us/"
       },
       {
          "name":"How Climate Change Has Disrupted The Environment",
-         "url":"./audio/How climate change has disrupted the environment 2.mp3",
+         "url":"./audio/How Climate change has disrupted the environment 2.mp3",
          "source": "United Nations",
-         "source_url": "https://www.who.int/publications/i/item/9789240003927"
+         "source_url": "https://edenconserve.org/about-us/"
       },
       {
          "name":"Health and Climate Change",
-         "url":"./audio/Health and Climate Change.mp3",
+         "url":"./audio/Health and Climate change.mp3",
          "source": "United Nations",
-         "source_url": "https://www.who.int/publications/i/item/9789240003927"
+         "source_url": "https://edenconserve.org/about-us/"
       },
       {
          "name":"How Can We Fight Climate Change",
          "url":"./audio/How can we fight climate change.mp3",
          "source": "United Nations",
-         "source_url": "https://www.who.int/publications/i/item/9789240003927"
+         "source_url": "https://edenconserve.org/about-us/"
       },
       {
          "name":"Contact us",
          "url":"./audio/Contact Us.mp3",
          "source":"United Nations",
-         "source_url": "https://www.who.int/publications/i/item/9789240003927"
+         "source_url": "https://edenconserve.org/about-us/"
       },
    ]
 });
